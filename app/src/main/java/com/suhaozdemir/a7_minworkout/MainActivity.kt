5c3f4e03093535f3fd2cef94364e79ccab520628
suhaozdemir <ahmetsuhaozdemir@gmail.com>
2022-08-04 11:45:26 +0300

    Finished MainActivity UI

--- app/src/main/java/com/suhaozdemir/a7_minworkout/MainActivity.kt
+++ app/src/main/java/com/suhaozdemir/a7_minworkout/MainActivity.kt
@@ -2,10 +2,18 @@ package com.suhaozdemir.a7_minworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+
+        flStartButton.setOnClickListener{
+            Toast.makeText(this, "Here we go", Toast.LENGTH_LONG).show()
+        }
     }
 }
