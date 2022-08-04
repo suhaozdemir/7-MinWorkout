6b84a1430fef98efc4cf11fb022ce060c37f1962
suhaozdemir <ahmetsuhaozdemir@gmail.com>
2022-08-04 11:58:18 +0300

    Added ViewBinding

--- app/src/main/java/com/suhaozdemir/a7_minworkout/MainActivity.kt
+++ app/src/main/java/com/suhaozdemir/a7_minworkout/MainActivity.kt
@@ -2,17 +2,20 @@ package com.suhaozdemir.a7_minworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
 import android.widget.Toast
+import com.suhaozdemir.a7_minworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val flStartButton : FrameLayout = findViewById(R.id.flStart)
-
-        flStartButton.setOnClickListener{
+        //val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        binding.flStart.setOnClickListener{
             Toast.makeText(this, "Here we go", Toast.LENGTH_LONG).show()
         }
     }
