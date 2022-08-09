e9f4b2ed18bfaf2a781f8798700327fb44a6ac50
suhaozdemir <ahmetsuhaozdemir@gmail.com>
2022-08-09 22:36:21 +0300

    Added Timer to ExerciseProgressBar

--- app/src/main/java/com/suhaozdemir/a7_minworkout/ExerciseActivity.kt
+++ app/src/main/java/com/suhaozdemir/a7_minworkout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.suhaozdemir.a7_minworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.suhaozdemir.a7_minworkout.databinding.ActivityExerciseBinding
 
@@ -11,7 +12,9 @@ class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
 
     private var restTimer : CountDownTimer? = null
+    private var exerciseTimer : CountDownTimer? = null
     private var restProgress = 0
+    private var exerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,24 +44,52 @@ class ExerciseActivity : AppCompatActivity() {
                     "Starting your exercise",
                     Toast.LENGTH_SHORT
                 ).show()
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar(){
+        binding.exerciseProgressBar.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(30000, 1000){
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding.exerciseProgressBar.progress = 30 - exerciseProgress
+                binding.tvExerciseTimer.text = (30 - exerciseProgress).toString()
+            }
+            override fun onFinish() {
+
             }
         }.start()
     }
 
     private fun setupRestView(){
+        reset()
+        setRestProgressBar()
+    }
+
+    private fun setupExerciseView(){
+        binding.progressBar.visibility = View.GONE
+        binding.tvTitle.text = "Exercise Name"
+        binding.flExerciseProgressBar.visibility = View.VISIBLE
+        reset()
+        setExerciseProgressBar()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        reset()
+    }
+
+    private fun reset(){
         if(restTimer != null){
             restTimer?.cancel()
             restProgress = 0
         }
-        setRestProgressBar()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        if(restTimer != null){
-            restTimer?.cancel()
-            restProgress = 0
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
         }
     }
 
