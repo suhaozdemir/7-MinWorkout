c988768b81f4d3605f381aa0f7f39fb2ade1dd05
suhaozdemir <ahmetsuhaozdemir@gmail.com>
2022-08-09 14:48:50 +0300

    Added Timer to ProgressBar

--- app/src/main/java/com/suhaozdemir/a7_minworkout/ExerciseActivity.kt
+++ app/src/main/java/com/suhaozdemir/a7_minworkout/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package com.suhaozdemir.a7_minworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.suhaozdemir.a7_minworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityExerciseBinding
+
+    private var restTimer : CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,42 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbarExercise.setNavigationOnClickListener{
             onBackPressed()
         }
+
+        setupRestView()
     }
+
+    private fun setRestProgressBar(){
+        binding.progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding.progressBar.progress = 10 - restProgress
+                binding.tvTimer.text = (10 - restProgress).toString()
+            }
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,
+                    "Starting your exercise",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+    }
+
 }
